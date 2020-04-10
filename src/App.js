ee36383c5f3e6ed3e5caa4f3491ccb852f71cdd7
Levi Donaldson <lcdbacanas@gmail.com>
2020-04-10 01:16:28 -0500

    state and ref changes to app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React, {Component} from 'react';
+import React, {useState, useRef} from 'react';
 import Button from './Components/Common/Button/Button.js';
 import Card from './Components/Common/Card/Card.js';
 import './App.css';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      users: [],
-      toastMessage: ""
-    };
-  }
+const App = () => {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     users: [],
+  //     toastMessage: ""
+  //   };
+  // }
 
-  componentDidMount = () => {
-    this.getUsers()
-  }
+  // componentDidMount = () => {
+  //   this.getUsers()
+  // }
+  const [users, setUsers] = useState(0);
+  const [toastMessage, setToastMessage] = useState();
 
-  returnToast = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+
+  const returnToast = () => {
     document.getElementById('toast').style.visibility = "visible";
     setTimeout(() => {
       document.getElementById('toast').style.visibility = "hidden";
     }, 2500);
   }
 
-  getUsers = async () => {
+  const getUsers = async () => {
     const response = await fetch('http://localhost:3300/users/', {
       headers: {
         'Access-Control-Allow-Origin': '*',
@@ -32,15 +37,18 @@ class App extends Component {
       }
     })
     const json = await response.json();
-    this.setState({
-      users: json
-    });
+    // this.setState({
+    //   users: json
+    // });
+    setUsers({users: json});
   }
 
-  addUser = async () => {
-    const name = document.getElementById('name').value
-    const email = document.getElementById('email').value
-
+  const addUser = async () => {
+    // const name = document.getElementById('name').value
+    // const email = document.getElementById('email').value
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    console.log(name, email);
     const url = `http://localhost:3300/users`;
     const config = { 
       method: 'POST',
@@ -50,25 +58,30 @@ class App extends Component {
         'Accept': 'application/json;charset=UTF-8'
       },
       body: JSON.stringify({
-        name: name,
-        email: email
+        // name: name,
+        // email: email
       })
     }
+    
     await fetch(url, config)
     .then(response => {
       if (!response.status === 200 || !response.status === 201) {
         throw Error(response.message);
       } else {
-        return this.returnToast();
+        return returnToast();
       }
+      console.log(JSON.stringify(response, null, 2));
     });
-    this.setState({ toastMessage: "New User Added" });
-    this.getUsers();
+    // this.setState({ toastMessage: "New User Added" });
+    setToastMessage(toastMessage, "New User Added");
+    getUsers();
   }
 
-  deleteUser = (id) => {
+  const deleteUser = (id) => {
     const url = `http://localhost:3300/users/${id}`
-    const {users} = this.state;
+    // const {users} = this.state;
+    setUsers(users);
+
     const index = users.findIndex(user => user.id === id);
 
     const config = {
@@ -89,63 +102,66 @@ class App extends Component {
       if (index === -1) return;
       users.splice(index, 1);
 
-      this.setState({ users, toastMessage: "User Deleted"});
-      this.returnToast();
+      // this.setState({ users, toastMessage: "User Deleted"});
+      setToastMessage(toastMessage, "User Deleted");
+      returnToast();
   }
 
-  render() {
-    const {users, toastMessage} = this.state
+  // render() {
+  //   const {users, toastMessage} = this.state
 
-    return (
-      <div>
-        <h1 className="Greeting">Hello Levi </h1>
-        <h2 className="Title">Here the users</h2>
-        <p id="toast" className="toastStyle">{toastMessage}</p>
+  return (
+    <div>
+      <h1 className="Greeting">Hello Levi </h1>
+      <h2 className="Title">Here the users</h2>
+      <p id="toast" className="toastStyle">{toastMessage}</p>
 
-        <div className="form-wrapper">
-          <div>
-            <label>Name</label>
-            <input
-              id="name"
-              type="text"
-              name="name"
-              placeholder="Name"></input>
-          </div>
-          <div>
-            <label>Email</label>
-            <input
-              id="email"
-              type="text"
-              name="name"
-              placeholder="Name"></input>
-          </div>
-          <div className="btn-wrapper">
-            <Button
-              btnStyle="btn--success--outline"
-              btnSize="btn--sm"
-              onClick={this.addUser}
-            >Add a new user</Button>
-          </div>
+      <div className="form-wrapper">
+        <div>
+          <label>Name</label>
+          <input
+            ref={nameRef}
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Name"></input>
         </div>
-
-        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
-          {users.map(user => (
-            <div style={{flex: 1}}>
-            <Card
-              cardStyle="card--primary--outline br4"
-            // cardSize="card--md"
-              key={user.id}
-              id={user.id}
-              name={user.name}
-              email={user.email}
-              exit={() => {this.deleteUser(user.id)}}
-            />
-            </div>
-          ))}
+        <div>
+          <label>Email</label>
+          <input
+            ref={emailRef}
+            id="email"
+            type="text"
+            name="name"
+            placeholder="Name"></input>
+        </div>
+        <div className="btn-wrapper">
+          <Button
+            btnStyle="btn--success--outline"
+            btnSize="btn--sm"
+            onClick={addUser}
+          >Add a new user</Button>
         </div>
       </div>
-    );
-  }
+
+      <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+        {[users].map(user => (
+          <div style={{flex: 1}}>
+          <Card
+            cardStyle="card--primary--outline br4"
+          // cardSize="card--md"
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            email={user.email}
+            exit={() => {deleteUser(user.id)}}
+          />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  // }
 }
 
 export default App;
